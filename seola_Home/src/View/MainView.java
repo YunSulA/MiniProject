fe2c6764a386649a2ae1f7457f425ddc7422b52c
peron <peron@210.105.169.211>
2019-01-21 21:32:48 +0900

    메인뷰 화면 가운데 오게 고정

--- seola_Home/src/View/MainView.java
+++ seola_Home/src/View/MainView.java
@@ -1,7 +1,9 @@
 package View;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,15 +13,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainView extends JFrame implements ActionListener {
-
+	Toolkit tk = Toolkit.getDefaultToolkit();
 	Button01 Button01 = new Button01();
 	JButton chung1 = Button01.getButton();
 	JFrame frame = new JFrame("박성정 선생님 취업반");
 	private JPanel panel = new MyPanel();
-
+	static int f_width = 1400;
+	static int f_height = 970;
 
 	MainView() {
-    	
+		Dimension screen = tk.getScreenSize();
+		int f_xpos = (int) (screen.getWidth() / 2 - f_width / 2);
+		int f_ypos = (int) (screen.getHeight() / 2 - f_height / 2);
+		frame.setLocation(f_xpos, f_ypos);
         frame.setLayout(null);
         frame.setSize(1400,970);
         frame.setVisible(true);
