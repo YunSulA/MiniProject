2b2e79623b10da58fde14e80c4100e1ba8e87f3d
pc <pc@DESKTOP-RV9J6SP>
2019-01-21 11:33:43 +0900

    모델 작성완료

--- Cheong/src/Academy/DAO/Academy_DAO.java
+++ Cheong/src/Academy/DAO/Academy_DAO.java
@@ -18,12 +18,23 @@ public class Academy_DAO {
 		PreparedStatement pstmt = null;
 		
 		try {
-			pstmt = conn.prepareStatement("");
+			pstmt = conn.prepareStatement("INSERT INTO ACADEMY VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			pstmt.setString(1, academy.getName());
+			pstmt.setString(2, academy.getBirth());
+			pstmt.setString(3, academy.getTel());
+			pstmt.setString(4, academy.getAddress());
+			pstmt.setString(5, academy.getMajor());
+			pstmt.setString(6, academy.getItcourse());
+			pstmt.setInt(7, academy.getTribute());
+			pstmt.setInt(8, academy.getScores());
+			pstmt.setInt(9, academy.getAttend());
+			pstmt.setString(10, academy.getSurvey());
 			// 이름 . 생년월일. 전화번호. 주소. 전공. 단과반유무. 조공횟수. 성적. 출석. 설문조사
 	// name. birth. tel. address. major. itcourse. tribute. scores. attend. survey
 	// 외부 테이블이 존재하는 항목 : scores. attend 				
 					
-			return pstmt.executeUpdate();		
+			return pstmt.executeUpdate();	
+			
 		} finally {
 			DBResourceReturn.close(pstmt);
 		}
--- Cheong/src/Academy/model/Academy.java
+++ Cheong/src/Academy/model/Academy.java
@@ -2,4 +2,93 @@ package Academy.model;
 
 public class Academy {
 
+	//name. birth. tel. address. major. itcourse. tribute. scores. attend. survey
+	private String name;
+	private String birth;
+	private String tel;
+	private String address;
+	private String major;
+	private String itcourse;
+	private int tribute;	// String으로 변경할 수도 ... 품목을 적는다면 0ㅁ 0
+	private int scores;
+	private int attend;
+	private String survey;
+	
+	public Academy() { }
+	public Academy(String name, String birth, String tel, String address, String itcourse, int tribute, int scores, int attend, String survey) {
+		this.name = name;
+		this.birth = birth;
+		this.tel = tel;
+		this.address = address;
+		this.itcourse = itcourse;
+		this.tribute = tribute;
+		this.scores = scores;
+		this.attend = attend;
+		this.survey = survey;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getItcourse() {
+		return itcourse;
+	}
+	public void setItcourse(String itcourse) {
+		this.itcourse = itcourse;
+	}
+	public int getTribute() {
+		return tribute;
+	}
+	public void setTribute(int tribute) {
+		this.tribute = tribute;
+	}
+	public int getScores() {
+		return scores;
+	}
+	public void setScores(int scores) {
+		this.scores = scores;
+	}
+	public int getAttend() {
+		return attend;
+	}
+	public void setAttend(int attend) {
+		this.attend = attend;
+	}
+	public String getSurvey() {
+		return survey;
+	}
+	public void setSurvey(String survey) {
+		this.survey = survey;
+	}
+	
+	
+	
 }
