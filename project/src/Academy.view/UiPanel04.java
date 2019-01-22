998163c87828792dc2f10445139351da88db0b92
pc <pc@DESKTOP-RV9J6SP>
2019-01-22 09:34:57 +0900

    패키지명 변경. 프로젝트1 삭제

---
+++ project/src/Academy.view/UiPanel04.java
@@ -0,0 +1,90 @@
+package view;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import view.UiPanel01.MyActionListener;
+
+public class UiPanel04 extends JPanel {
+	private JPanel panel04 = new MyPanel04();
+	private UiChange win;
+	private JButton menu1;
+	private JButton menu2;
+	private JButton menu3;
+	private JButton menu4;
+
+
+	public UiPanel04(UiChange win) {
+		this.win = win;
+		setSize(750, 540);
+		setVisible(true);
+		setLayout(null);
+		add(panel04);
+
+		panel04.setSize(750, 540);
+		panel04.setLayout(null);
+
+		menu1 = new JButton();
+		menu1.setBounds(40, 40, 140, 80);
+		menu1.setContentAreaFilled(false);// 버튼 내용영역 안채움
+		menu1.setFocusPainted(false);
+		menu1.setBorderPainted(false);
+		panel04.add(menu1);
+
+		menu2 = new JButton();
+		menu2.setBounds(40, 160, 140, 80);
+		menu2.setContentAreaFilled(false);// 버튼 내용영역 안채움
+		menu2.setFocusPainted(false);
+		menu2.setBorderPainted(false);
+		panel04.add(menu2);
+
+		menu3 = new JButton();
+		menu3.setBounds(40, 300, 140, 80);
+		menu3.setContentAreaFilled(false);// 버튼 내용영역 안채움
+		menu3.setFocusPainted(false);
+		menu3.setBorderPainted(false);
+		panel04.add(menu3);
+
+		menu4 = new JButton();
+		menu4.setBounds(40, 420, 140, 80);
+		menu4.setContentAreaFilled(false);// 버튼 내용영역 안채움
+		menu4.setFocusPainted(false);
+		menu4.setBorderPainted(false);
+		panel04.add(menu4);
+
+		menu1.addActionListener(new MyActionListener());
+		menu2.addActionListener(new MyActionListener());
+		menu3.addActionListener(new MyActionListener());
+		menu4.addActionListener(new MyActionListener());
+	}
+
+	class MyPanel04 extends JPanel {
+		@Override
+		protected void paintComponent(Graphics g) {
+			Image img = new ImageIcon("img\\UI01_4.png").getImage();
+			g.drawImage(img, 0, 0, 750, 540, null);
+		}
+	}
+
+	class MyActionListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == menu1) {
+				win.change("uipanel01");
+			} else if (e.getSource() == menu2) {
+				win.change("uipanel02");
+			} else if (e.getSource() == menu3) {
+				win.change("uipanel03");
+			} else if (e.getSource() == menu4) {
+				win.change("uipanel04");
+			}
+		}
+
+	}
+}
